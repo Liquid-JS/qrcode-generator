b2629b1138c42682c9749f6b98df380a3da715a3
HitkoDev <development@hitko.si>
2026-04-08 08:06:18 +0200

    ci: docs

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,7 @@ export default defineConfig(
     {
         files: ['**/*.ts', '**/*.js', '**/*.cjs'],
         plugins: {
+            // @ts-ignore
             '@typescript-eslint': typescriptEslintPlugin,
             '@import': importPlugin,
             '@jsdoc': jsdocPlugin,
